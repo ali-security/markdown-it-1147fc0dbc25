0c57fb10d56fea72b045e59a2482cf62a64b005b
Seal <info@sealsecurity.io>
2026-08-14 14:52:07 +0000

    CVE-2026-48988

--- test/pathological.mjs
+++ test/pathological.mjs
@@ -154,5 +154,9 @@ describe('Pathological sequences speed', () => {
         { linkify: true }
       )
     })
+
+    it('a lot of smartquotes', async () => {
+      await test_pattern('"'.repeat(160000), { typographer: true })
+    })
   })
 })
